1d5e0c0e323f78ec6c16b7df967fa399c48847ca
Anjali J Kumar <kumaranjalij@gmail.com>
2024-04-30 20:33:37 +0530

    Dashboard UI final

--- frontend/dnexus/src/components/Navbar/Navbar.jsx
+++ frontend/dnexus/src/components/Navbar/Navbar.jsx
@@ -1,57 +1,60 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
-import "./Navbar.css"
+import { NavLink, useNavigate } from "react-router-dom";
 
 import logoImage from "../assets/logo/nexus blue-crop.png";
 
+import "./Navbar.css";
+
 const Navbar = () => {
 
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  const handleClick = () => {
-    // Navigate to the new page
-    navigate("/LoginAs");
-    console.log("handleclick called");
-  };
+    const handleClick = () => {
+      // Navigate to the new page
+      navigate("/LoginAs");
+      console.log("handleclick called");
+    };
 
-  return (
-    <div className="header">
-      <nav className="navbar-nav">
+    return(
         <div className="navbar-container">
-          <NavLink to="/" className="navbar-logo">
-            <img className="logo" src={logoImage} alt="Logo" />
-          </NavLink>
+            <div className="nav">
+
+                <div className="logo">
+                <NavLink to="/" className="navbar-logo">
+                    <img className="logo" src={logoImage} alt="Logo" />
+                </NavLink>
+                </div>
+
+                <div className="nav-menu">
+                    <ul>
+                        <li>
+                            <NavLink to="/about-us" className="nav-links">
+                                About Us
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/about-us" className="nav-links">
+                                Privacy Policy
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/about-us" className="nav-links">
+                                Contact Us
+                            </NavLink>
+                        </li>
+                        <li>
+                            <button className="login-btn" 
+                                buttonText="LOGIN"
+                                onClick={handleClick}>
+                                LOGIN
+                            </button>
+                        </li>
+                    </ul>
+                </div>
+
+            </div>
         </div>
-        <div className="list">
-          <ul className="nav-menu">
-            <li className="nav-item">
-              <NavLink to="/about-us" className="nav-links">
-                About Us
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink to="/privacy-policy" className="nav-links">
-                Privacy Policy
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink to="/contact" className="nav-links">
-                Contact Us
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <button className="login-btn" 
-                  buttonText="LOGIN"
-                  onClick={handleClick}>
-                  LOGIN
-              </button>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    </div>
-  );
-};
+    )
+}
 
 export default Navbar;
--- frontend/dnexus/src/pages/LandingPage.jsx
+++ frontend/dnexus/src/pages/LandingPage.jsx
@@ -19,56 +19,54 @@ function LandingPage() {
   return (
     <div>
       <ConditionalNavbar />
+
       {/* <MySlider/> */}
       {/* <!-- Slider Area --> */}
-		<section className="slider">
-			<div className="hero-slider">
-				{/* <!-- Start Single Slider --> */}
-				<div className="single-slider" style={{backgroundImage: `url(${require('../components/assets/StockImages/slider3.jpg')})`}}>
-					<div className="container">
-						<div className="row">
-							<div className="col-lg-7">
-								<div className="text">
-									<h1>Bridging <span>Expertise</span> Empowering <span>Healthcare!</span></h1>
-									<p>Empowering Precision Healthcare Through Seamless Collaboration and Data-Driven Insights! </p>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>			
-				{/* <!-- End Single Slider --> */}
-			</div>
-		</section>
-		{/* <!--/ End Slider Area --> */}
+      <section className="slider">
+        <div className="hero-slider">
+          {/* <!-- Start Single Slider --> */}
+          <div className="single-slider" style={{backgroundImage: `url(${require('../components/assets/StockImages/slider3.jpg')})`}}>
+            <div className="container">
+              <div className="row">
+                <div className="col-lg-7">
+                  <div className="text">
+                    <h1>Bridging <span>Expertise</span> Empowering <span>Healthcare!</span></h1>
+                    <p>Empowering Precision Healthcare Through Seamless Collaboration and Data-Driven Insights! </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>			
+          {/* <!-- End Single Slider --> */}
+        </div>
+      </section>
+      {/* <!--/ End Slider Area --> */}
+
       <Funfact/>
 
+      <div className="appointment">
 
-      <section className="appointment" style={{ position: 'relative' }}>
-      <div className="container">
-                <div className="row">
-                    <div className="col-lg-6 col-md-12">
-                        <div className="appointment-image">
-                            <img src={contactImg} alt="#" />
-                        </div>
-                    </div>
-                    <div className="col-lg-6 col-md-12">
-                        <div className="section-title">
-                            <h2>Revolutionizing Healthcare Collaboration: Connect, Collaborate, and Care with Confidence!</h2>
-                            <img src={sectionImg} alt="#" />
-                            <br/>
-                            <p style={{ padding: '25px' }}>Our platform empowers healthcare providers to efficiently create and manage cases for diagnostic tests, ensuring streamlined workflows and accurate data management. From uploading test results to accessing DICOM images, our platform offers a comprehensive solution for medical professionals.
+        <div className="image">
+          <img src={contactImg} alt="#" />
+        </div>
 
-With our secure environment, radiologists and doctors can securely view DICOM images, analyze results, and collaborate in real-time discussions. Our platform prioritizes privacy and security, ensuring that patient data remains protected at all times.
+        <div className="text">
+          <div className="section-title">
+              <h2>Revolutionizing Healthcare Collaboration: Connect, Collaborate, and Care with Confidence!</h2>
+              <img src={sectionImg} alt="#" />
+              <br/>
+              <p style={{ padding: '25px' }}>Our platform empowers healthcare providers to efficiently create and manage cases for diagnostic tests, ensuring streamlined workflows and accurate data management. From uploading test results to accessing DICOM images, our platform offers a comprehensive solution for medical professionals.
+                        With our secure environment, radiologists and doctors can securely view DICOM images, analyze results, and collaborate in real-time discussions. Our platform prioritizes privacy and security, ensuring that patient data remains protected at all times.
+                        Experience the future of healthcare collaboration with DNexus, where innovation meets security for enhanced patient care.
+              </p>
+          </div>
 
-Experience the future of healthcare collaboration with DNexus, where innovation meets security for enhanced patient care.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
+        </div>
 
+      </div>
 
       <Footer/>
+      
     </div>
   );
 }
