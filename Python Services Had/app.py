e92354a81ea47903189608c29bf091d828a425bb
SambhuPNS <sambhusubramannian@gmail.com>
2024-05-05 22:04:07 +0530

    End point for transcribing is also done

--- Python Services Had/app.py
+++ Python Services Had/app.py
@@ -1,5 +1,12 @@
 from flask import Flask, request, jsonify
 from transformers import pipeline
+from flask import Flask, request, jsonify
+import io
+import os
+from pydub import AudioSegment
+from google.cloud import speech_v1p1beta1 as speech
+
+
 
 # Initialize Flask app
 app = Flask(__name__)
@@ -21,6 +28,80 @@ def summarize_text():
 
     # Return the summarized text
     return jsonify({'summary': summary_text})
+
+
+@app.route('/transcribe', methods=['POST'])
+def transcribe_endpoint():
+    # Check if the file is included in the request
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file provided'})
+
+    file = request.files['file']
+
+    # Check if the file is empty
+    if file.filename == '':
+        return jsonify({'error': 'Empty file provided'})
+
+    # Save the file temporarily
+    input_file = 'temp_audio.wav'
+    file.save(input_file)
+
+    # Convert and transcribe
+    try:
+        transcribed_text = convert_and_transcribe(input_file)
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
+    # Remove the temporary file
+    os.remove(input_file)
+
+    return jsonify({'transcription': transcribed_text})
+
+
+def convert_and_transcribe(input_file):
+    # Load the WAV audio file
+    audio = AudioSegment.from_wav(input_file)
+
+    # Resample to 16 kHz sample rate
+    audio = audio.set_frame_rate(16000)
+
+    # Export the audio with the new sample rate
+    output_file = 'output_audio_16000hz.wav'
+    audio.export(output_file, format="wav")
+
+    # Read the WAV audio file
+    with io.open(output_file, "rb") as audio_file:
+        content = audio_file.read()
+
+    # Initialize Speech-to-Text client
+    os.environ[
+        "GOOGLE_APPLICATION_CREDENTIALS"] = "concise-hue-414010-0ca7d551bc7f.json"  # Replace with your service account key file path
+    client = speech.SpeechClient()
+
+    # Specify audio file type and sample rate hertz
+    audio = speech.RecognitionAudio(content=content)
+    config = speech.RecognitionConfig(
+        encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
+        sample_rate_hertz=16000,
+        language_code="en-US",
+    )
+
+    # Perform the transcription
+    response = client.recognize(config=config, audio=audio)
+
+    # Extract transcription
+    transcribed_text = ""
+    for result in response.results:
+        transcribed_text += result.alternatives[0].transcript.title() + " "
+
+    # Clean up temporary WAV file
+    os.remove(output_file)
+
+    return transcribed_text
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
 if __name__ == '__main__':
     # Run the Flask app
